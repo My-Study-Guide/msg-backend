8192dc9c69f152f53185d12b2fa38009e52afb4b
hera <ansaudwls95@naver.com>
2024-09-15 21:53:47 +0900

    refactor: 안 쓰는 코드 삭제

--- msg_api/hello/__init__.py
+++
@@ -1,6 +0,0 @@
-from .controller.hello import HelloController
-from flask import Blueprint
-
-hello_bp = Blueprint("hello", __name__, url_prefix="/api/hello")
-
-hello_bp.add_url_rule("/", view_func=HelloController.as_view("hello"))
