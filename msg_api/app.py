bb79a384e012181d4cffa688b938d88d5dc7acde
hera <ansaudwls95@naver.com>
2024-09-15 22:58:40 +0900

    CORS 추가

--- msg_api/app.py
+++ msg_api/app.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restx import Api
+
+from msg_api import config
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.PROPAGATE_EXCEPTIONS = True
+    app.config.from_object(config)
 
     api = Api(app=app)
     register_namespaces(api)
+    CORS(app)
     initialize()
     return app
 
